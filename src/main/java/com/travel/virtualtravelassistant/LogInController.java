1d10ec258811fd202cff33e90dbc33b3a11bbec2
jatalamantes <talamantesja@gmail.com>
2024-03-18 19:50:30 -0400

    Login Page

---
+++ src/main/java/com/travel/virtualtravelassistant/LogInController.java
@@ -0,0 +1,15 @@
+package com.travel.virtualtravelassistant;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LogInController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
