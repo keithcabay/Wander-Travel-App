8a7aa697ef5fdf0aebbc98e547ac0c7e28a87735
Keith <keithwcabay@gmail.com>
2024-03-22 10:26:37 -0400

    Create files for trip preview card

---
+++ src/main/java/com/travel/virtualtravelassistant/Trip.java
@@ -0,0 +1,53 @@
+package com.travel.virtualtravelassistant;
+
+public class Trip {
+    private String imageSource;
+
+    private String countryName;
+
+    private String cityName;
+
+    private double budget;
+
+    private int length;
+
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    public void setImageSource(String imageSource) {
+        this.imageSource = imageSource;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
---
+++ src/main/java/com/travel/virtualtravelassistant/TripPreviewCardController.java
@@ -0,0 +1,31 @@
+package com.travel.virtualtravelassistant;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class TripPreviewCardController {
+    @FXML
+    private ImageView imageOfDestination;
+
+    @FXML
+    private Label destinationName;
+
+    @FXML
+    private Label tripBudget;
+
+    @FXML
+    private Label tripLength;
+
+    public void setTrip(Trip trip){
+        Image image = new Image(getClass().getResourceAsStream(trip.getImageSource()));
+        imageOfDestination.setImage(image);
+        imageOfDestination.setPreserveRatio(false);
+        imageOfDestination.setOpacity(0.65);
+        imageOfDestination.setFitWidth(600);
+        destinationName.setText(trip.getCityName() + ", " + trip.getCountryName());
+        tripBudget.setText(String.valueOf("$" + trip.getBudget()));
+        tripLength.setText(String.valueOf(trip.getLength()) + " Days");
+    }
+}
