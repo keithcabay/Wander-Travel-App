c86494cbea157ddf2f1769e8b6eb584246843cce
Keith Cabay <keith.cabay10@gmail.com>
2024-03-25 15:42:24 -0400

    Add UID to store in cache and get full name method

--- src/main/java/com/travel/virtualtravelassistant/User/UserInfo.java
+++ src/main/java/com/travel/virtualtravelassistant/User/UserInfo.java
@@ -2,6 +2,7 @@ package com.travel.virtualtravelassistant.User;
 
 
 public class UserInfo {
+    private String UID;
 
    private String first_name;
 
@@ -9,11 +10,24 @@ public class UserInfo {
 
    private String email;
 
-   public UserInfo(String first_name, String last_name, String email){
+   public UserInfo(String UID, String first_name, String last_name, String email){
+        this.UID = UID;
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
    }
+
+   public String getFullName(){
+       return first_name + " " + last_name;
+   }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
 
     public String getFirst_name() {
         return first_name;
