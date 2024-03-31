4c52a59f7df5e0121fe96044b4c19562ab8c7ec9
jatalamantes <talamantesja@gmail.com>
2024-03-31 19:03:22 -0400

    adding

--- src/main/java/com/travel/virtualtravelassistant/PlanNextTrip/PlanNextTripController.java
+++ src/main/java/com/travel/virtualtravelassistant/PlanNextTrip/PlanNextTripController.java
@@ -16,8 +16,14 @@ public class PlanNextTripController implements Initializable {
     @FXML
     private TextField locationTextField;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Initialization code here
+    }
+
     @FXML
     private void searchLocation() {
+
         String apiKey = "2175F302BAC646F9991CD7DF16E2ECA6";
         String locationId = "19526673";  // locationTextField.getText(); // Get the location input from the text field
         String apiUrl = "https://api.tripadvisor.com/data/1.0/location/" + locationId + "/reviews?key=" + apiKey;
