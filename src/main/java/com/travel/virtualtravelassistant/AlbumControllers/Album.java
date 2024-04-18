35b1e7a6df23b4e3c58576d48d626ce7b6784ca2
Keith <keithwcabay@gmail.com>
2024-04-18 00:07:02 -0400

    Modified to Album model class

---
+++ src/main/java/com/travel/virtualtravelassistant/AlbumControllers/Album.java
@@ -0,0 +1,69 @@
+package com.travel.virtualtravelassistant.AlbumControllers;
+
+import com.google.cloud.ByteArray;
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album {
+
+    private String firestoreId;
+
+    private String localPathToCover;
+
+    private UserImage albumCover;
+
+    private Image currentAlbumCoverImage;
+
+    private String title;
+
+    private List<UserImage> images;
+
+    private List<ByteArray> loaded;
+
+    public Album(){
+        images = new ArrayList<>();
+    }
+
+    public String getFirestoreId() {
+        return firestoreId;
+    }
+
+    public void setFirestoreId(String firestoreId) {
+        this.firestoreId = firestoreId;
+    }
+
+    public UserImage getAlbumCover(){
+        return albumCover;
+    }
+
+    public void setAlbumCover(UserImage albumCover){
+        this.albumCover = albumCover;
+        images.add(albumCover);
+    };
+
+    public Image getCurrentAlbumCoverImage(){
+        return currentAlbumCoverImage;
+    }
+
+    public void setCurrentAlbumCoverImage(Image image){
+        this.currentAlbumCoverImage = image;
+    }
+
+    public String getLocalPathToCover() {
+        return localPathToCover;
+    }
+    public void setLocalPathToCover(String localPathToCover) {
+        this.localPathToCover = localPathToCover;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
