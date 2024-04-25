0669008ca090531389664ac6407c0791597b7c04
Keith <keithwcabay@gmail.com>
2024-04-25 01:16:51 -0400

    Added methods to send/add and get notifications and friends to/from firestore

--- src/main/java/com/travel/virtualtravelassistant/Utility/FirestoreAction.java
+++ src/main/java/com/travel/virtualtravelassistant/Utility/FirestoreAction.java
@@ -3,9 +3,12 @@ package com.travel.virtualtravelassistant.Utility;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.travel.virtualtravelassistant.AlbumControllers.Album;
+import com.travel.virtualtravelassistant.Inbox.Notification;
 import com.travel.virtualtravelassistant.MainApplication;
 import com.travel.virtualtravelassistant.User.CurrentUser;
 import com.travel.virtualtravelassistant.AlbumControllers.UserImage;
+import com.travel.virtualtravelassistant.User.UserInfo;
+import javafx.scene.image.Image;
 
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -147,5 +150,157 @@ public class FirestoreAction {
         }catch (Exception e){
             System.out.println("Could not add new register to DB");
         }
+    }
+
+
+    /***
+     * Send a message/notification and store in receivers collection
+     * Must set receiver email, sender email, and notification type
+     * @param notification the object storing info to store notification
+     */
+    public static void sendNotification(Notification notification) {
+        DocumentReference docRef = MainApplication.fstore.collection("Users")
+                .document(notification.getReceiverEmail());
+
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+
+        try{
+            DocumentSnapshot doc = future.get();
+            if(!doc.exists()){
+                System.out.println("Receiving user does not exist.");
+                return;
+            }else{
+                DocumentReference notiRef = docRef.collection("notifications").document();
+                Map<String, Object> notiMap = new HashMap<>();
+                notiMap.put("receiver-email", notification.getReceiverEmail());
+                notiMap.put("sender-email", notification.getSenderEmail());
+                notiMap.put("sender-first-name", notification.getSenderFirstName());
+                notiMap.put("sender-last-name", notification.getSenderLastName());
+                notiMap.put("notification-type", notification.getNotification_type());
+                notiMap.put("message", notification.getMessage());
+
+
+                notiRef.set(notiMap);
+            }
+        }catch (Exception e){
+            System.out.println("Failed to retrieve document in Firestore.");
+        }
+    }
+
+    public static List<Notification> getNotifications(){
+        List<Notification> notifications = new ArrayList<>();
+
+        CollectionReference notiRef = MainApplication.fstore.collection("Users")
+                .document(CurrentUser.getInstance().getUserInfo().getUID())
+                .collection("notifications");
+
+        ApiFuture<QuerySnapshot> future = notiRef.get();
+
+
+        try {
+            QuerySnapshot querySnapshot = future.get();
+            for (QueryDocumentSnapshot document : querySnapshot) {
+                String notification_id = document.getId();
+
+                Notification notification = new Notification();
+                notification.setNotification_id(notification_id);
+                notification.setNotification_type((String)document.get("notification-type"));
+                notification.setSenderEmail((String)document.get("sender-email"));
+                notification.setSenderFirstName((String)document.get("sender-first-name"));
+                notification.setSenderLastName((String)document.get("sender-last-name"));
+                notification.setMessage((String)document.get("message"));
+                notification.setReceiverEmail(CurrentUser.getInstance().getUserInfo().getUID());
+
+                notifications.add(notification);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Failed to retrieve albums from firebase.");
+            e.printStackTrace();
+        }
+        return notifications;
+    }
+
+    public static void deleteNotification(Notification notification){
+        DocumentReference notiRef = MainApplication.fstore.collection("Users")
+                .document(CurrentUser.getInstance().getUserInfo().getUID())
+                .collection("notifications")
+                .document(notification.getNotification_id());
+
+        notiRef.delete();
+    }
+
+    public static void addFriend(Notification notification){
+        DocumentReference friendsRef = MainApplication.fstore.collection("Users")
+                .document(CurrentUser.getInstance().getUserInfo().getUID())
+                .collection("friends").document();
+
+        Map<String, Object> friendMap = new HashMap<>();
+        friendMap.put("email", notification.getSenderEmail());
+        friendMap.put("first-name", notification.getSenderFirstName());
+        friendMap.put("last-name", notification.getSenderLastName());
+
+        friendsRef.set(friendMap);
+
+        friendsRef = MainApplication.fstore.collection("Users")
+                .document(notification.getSenderEmail().toLowerCase())
+                .collection("friends").document();
+
+        friendMap = new HashMap<>();
+        friendMap.put("email", CurrentUser.getInstance().getUserInfo().getUID());
+        friendMap.put("first-name", CurrentUser.getInstance().getUserInfo().getFirst_name());
+        friendMap.put("last-name", CurrentUser.getInstance().getUserInfo().getLast_name());
+        friendsRef.set(friendMap);
+    }
+
+
+
+    public static Set<UserInfo> getFriends(){
+        Set<UserInfo> friends = new HashSet<>();
+
+        CollectionReference notiRef = MainApplication.fstore.collection("Users")
+                .document(CurrentUser.getInstance().getUserInfo().getUID())
+                .collection("friends");
+
+        ApiFuture<QuerySnapshot> future = notiRef.get();
+
+
+        try {
+            QuerySnapshot querySnapshot = future.get();
+            for (QueryDocumentSnapshot document : querySnapshot) {
+                String userId = (String)document.get("email");
+
+                UserInfo userInfo = new UserInfo();
+                userInfo.setUID(userId);
+                userInfo.setFirst_name((String)document.get("first-name"));
+                userInfo.setLast_name((String)document.get("last-name"));
+
+                Image image = FirebaseStorageAction.getFriendProfilePicture(userId);
+                userInfo.setProfile_picture(image);
+
+                friends.add(userInfo);
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Failed to retrieve albums from firebase.");
+            e.printStackTrace();
+        }
+        return friends;
+
+    }
+    public static boolean doesUserExist(String email){
+        DocumentReference docRef = MainApplication.fstore.collection("Users")
+                .document(email);
+
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+
+        try {
+            DocumentSnapshot doc = future.get();
+            if (!doc.exists()) {
+                System.out.println("Receiving user does not exist.");
+                return false;
+            }
+        }catch (Exception e) {
+            System.out.println("Could not load document from firestore");
+        }
+        return true;
     }
 }
